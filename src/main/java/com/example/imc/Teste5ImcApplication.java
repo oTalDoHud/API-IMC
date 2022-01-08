c1bc92435a8fc9bfc076ca6bd69dc07c07ad264a
oTalDoHud <hudson.lucas.vieira@gmail.com>
2022-01-08 18:50:56 -0300

    first commit - test profile

---
+++ src/main/java/com/example/imc/Teste5ImcApplication.java
@@ -0,0 +1,13 @@
+package com.example.imc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Teste5ImcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Teste5ImcApplication.class, args);
+	}
+
+}
